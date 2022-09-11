130558a9013661eac1b9ddfac45d790c9221333a
DMITRIY <DMITRIY@MacBook-Pro.local>
2022-09-11 23:07:12 +0300

    Mobile version

--- components/MenuMobile.js
+++ components/MenuMobile.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useState } from "react";
 
 const MenuMobile = (props) => {
@@ -5,9 +6,13 @@ const MenuMobile = (props) => {
   return (
     <div className="w-full h-full z-10 bg-gray-900 bg-opacity-98 top-0 left-0 fixed sticky-0">
       <div className="2xl:container 2xl:mx-auto py-48 px-4 md:px-28 flex justify-center items-center">
+      <Link href="/">
+        <a>
         <h1 className="text-[32px] text-gray-400">
           hider<span className="text-lightGreen">lab.</span>
         </h1>
+        </a>
+        </Link>
         <div className="w-96 text-[32px] md:w-auto relative flex flex-col justify-center items-center py-16 px-4 md:px-24 xl:py-24 xl:px-36">
           {props.list}
         </div>
